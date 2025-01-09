1061d83b667560ea31518abcc5ab19e550c19a03
LemonManGui <124055131+LemonManGui@users.noreply.github.com>
2025-01-09 15:55:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import request
 
-url = https://european-digital-innovation-hubs.ec.europa.eu/edih-catalogue
+url = "https://european-digital-innovation-hubs.ec.europa.eu/edih-catalogue/"
 
 # Main page data file:
 main_page_data = pd.read_xls("export-edihs.xls")
 
 name_column = main_page_data["EDIH Name"]
 
-def get_name(name_column):
+def get_links(name_column):
   for i in len(name_column):
-    return i
+    new_list = url.append(i)
+    return new_list
+
+
