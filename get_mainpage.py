4f9bc4f37d957d67298555e84fc29f3f62c0c5ab
joepdeeben <joep.deeben@student.uva.nl>
2025-01-09 16:54:24 +0100

    comments

--- get_mainpage.py
+++ get_mainpage.py
@@ -4,10 +4,14 @@ import requests
 import openpyxl
 
 
-link = requests.get( "https://european-digital-innovation-hubs.ec.europa.eu/edih-catalogue/4pdih/")
-
-
 def download_list():
+    """
+    uses link and bs4 to download xlsx with main page info
+
+    :return:
+    main page .xlsx file with the main page data
+    """
+
     output_file = "clients.xlsx"
     mainpage = requests.get("https://european-digital-innovation-hubs.ec.europa.eu/edih-catalogue/")
     soup = BeautifulSoup(mainpage.text, "html.parser")
@@ -23,15 +27,22 @@ def download_list():
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while downloading the file: {e}")
 
- #hier maken we een lijst van clienten
+
 def get_clients(client_list):
+    """
+    :param client_list:
+    This is the .xlsx file with the main page data
+
+    :return:
+    returns a list of client names, for use in scraping individual pages
+    """
     df = pd.read_excel(f"{client_list}", index_col=None)
     df = df["EDIH Name"].tolist()
     print(df)
 
 
-
+if __name__ == '__main__':
+# call functions
 get_clients('clients.xlsx')
-
 download_list()
 
